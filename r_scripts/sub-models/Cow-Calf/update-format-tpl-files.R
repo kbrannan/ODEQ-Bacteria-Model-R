3146c6782758479050ae9e75251151753f9e992c
kbrannan <brannan.kevin@deq.state.or.us>
2016-01-15 10:57:50 -0800

    r script to modifies template files to new input file format. added new tpl files to repo

--- r_scripts/sub-models/Cow-Calf/update-format-tpl-files.R
+++ r_scripts/sub-models/Cow-Calf/update-format-tpl-files.R
@@ -7,7 +7,15 @@ chr.inp.f.01 <- scan(file = "cowcalf01.txt", sep = "\n", what = "character")
 chr.tpl.f.01 <- scan(file = "cowcalf01.tpl", sep = "\n", what = "character")
 
 ## get the rows of file that have PEST variables in them
-grep("^.*\\$.*\\$$", chr.tpl.f.01)
+lng.rows <- grep("^.*\\$.*\\$$", chr.tpl.f.01)
 
-
+# get files names of new input files
 chr.inp.f <- list.files(pattern = "*.new\\.txt")
+
+## for each subwtsd 02-18, replace lines in input file with pest variable names
+## from subwtsd 01 template file and save as *new.tpl file
+for(ii in 1:length(chr.inp.f)) {
+  tmp.tpl.f <- c("ptf $",scan(chr.inp.f[ii], sep = "\n", what = "character"))
+  tmp.tpl.f[lng.rows] <- chr.tpl.f.01[lng.rows]
+  cat(file = gsub("txt$","tpl",chr.inp.f[ii]), tmp.tpl.f, sep = "\n")
+}
