6eb9666b9e0304f58638cbaea1b7c426b8692cfb
kbrannan <brannan.kevin@deq.state.or.us>
2016-03-04 11:45:40 -0800

    adding input files to the script file

--- R_scripts/sub-models/Wildlife-Elk/Wildlife_Elk_Sub_Model_calculation_check-subwtsd-01.R
+++ R_scripts/sub-models/Wildlife-Elk/Wildlife_Elk_Sub_Model_calculation_check-subwtsd-01.R
@@ -5,5 +5,37 @@ source(paste0(chr.wildlife.elk.dir,"/Wildlife_Elk_Sub_Model.R"))
 df.output <- wildlifeElk(chr.wrkdir=chr.wildlife.elk.dir,chr.input="wildlifeelk01.txt")
 ##
 ## get input
-df.input <- read.table(file = paste0(chr.wildlife.elk.dir,"/wildlifeelk01.txt"), 
-                      comment.char = "*", sep = ":")
+## SQOLIM multiplcation factor: 
+chk.sqolim <- 9
+### two seasons
+chk.Season.1.Months -> c(11,12,1,2,3)
+chk.Season.2.Months -> c(4,5,6,7,8,9,10)
+### Habitats
+chk.Season.1.Pasture <- 365.29
+chk.Season.1.Forest <- 5700.35
+chk.Season1..RAOUT <- 1
+chk.Season.2.Pasture <- 64.55
+chk.Season.2.Fores <- 2135.41
+chk.Season.2,RAOUT <- 1
+### Percent of Landuse with Stream access
+chk.Season.1.Percent.Pasture.with.Stream.Access <- 25
+chk.Season.1.Percent.Forest.with.Stream.Access <- 50
+chk.Season.1.Percent.RAOUT.with.Stream.Access <- 0
+chk.Season.2.Percent.Pasture.with.Stream.Access <- 15
+chk.Season.2.Percent.Forest.with.Stream.Access <- 39
+chl.Season 2.Percent.RAOUT with stream.Access  - 0
+*** Animal Densities
+Season 1 Animal Density for Pasture in watershed (animal/ac):     3.0072900E-03
+Season 1 Animal Density for Forest in watershed (animal/ac):      3.0072900E-03
+Season 1 Animal Density for Residential/Agricultural Operations/Commercial/Urban/Transportation in watershed (animal/ac): 0.0
+Season 2 Animal Density for Pasture in watershed (animal/ac):     4.5000000E-02
+Season 2 Animal Density for Forest in watershed (animal/ac):      4.5000000E-02
+Season 2 Animal Density for Residential/Agricultural Operations/Commercial/Urban/Transportation in watershed (animal/ac): 0.0
+*** General parameters
+Season 1 Percent of animals on Pasture in and around streams:      9.9500000E+00
+Season 1 Percent of animals on Forest in and around streams:       1.9208850E+00
+Season 1 Percent of animals on Residential/Agricultural Operations/Commercial/Urban/Transportation in and around streams:  0
+Season 2 Percent of animals on Pasture in and around streams:      9.9500000E+00
+Season 2 Percent of animals on Forest in and around streams:       9.9500000E+00
+Season 2 Percent of animals on Residential/Agricultural Operations/Commercial/Urban/Transportation in and around streams:  0
+Bacteria Production of animal per day (orgs/day):     6.6500000E+08
