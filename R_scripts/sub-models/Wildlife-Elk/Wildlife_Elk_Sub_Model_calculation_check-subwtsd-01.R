bfa1bb343a7835e9868cf70ebf07d4a965c0f8c1
kbrannan <brannan.kevin@deq.state.or.us>
2016-03-07 14:20:26 -0800

    calculations up to elk in habitat with/without stream access

--- R_scripts/sub-models/Wildlife-Elk/Wildlife_Elk_Sub_Model_calculation_check-subwtsd-01.R
+++ R_scripts/sub-models/Wildlife-Elk/Wildlife_Elk_Sub_Model_calculation_check-subwtsd-01.R
@@ -11,12 +11,19 @@ chk.Bacteria.Prod <- 6.6500000E+08
 ### two seasons
 chk.Season.1.Months -> c(11,12,1,2,3)
 chk.Season.2.Months -> c(4,5,6,7,8,9,10)
+### Animal Densities
+chk.Season.1.Animal.Density.Pasture <- 3.0072900E-03
+chk.Season.1.Animal.Density.Forest  <- 3.0072900E-03
+chk.Season.1.Animal.Density.RAOUT   <- 0.0
+chk.Season.2.Animal.Density.Pasture <- 4.5000000E-02
+chk.Season.2.Animal.Density.Forest  <- 4.5000000E-02
+chk.Season.2.Animal.Density.RAOUT   <- 0.0
 ### Habitats
 chk.Season.1.Pasture <- 365.29
 chk.Season.1.Forest  <- 5700.35
-chk.Season1.RAOUT    <- 1
+chk.Season.1.RAOUT    <- 1
 chk.Season.2.Pasture <- 64.55
-chk.Season.2.Fores   <- 2135.41
+chk.Season.2.Forest   <- 2135.41
 chk.Season.2.RAOUT   <- 1
 ### Percent of Landuse with Stream access
 chk.Season.1.Percent.Pasture.with.Stream.Access <- 25
@@ -25,13 +32,6 @@ chk.Season.1.Percent.RAOUT.with.Stream.Access   <- 0
 chk.Season.2.Percent.Pasture.with.Stream.Access <- 15
 chk.Season.2.Percent.Forest.with.Stream.Access  <- 39
 chk.Season.2.Percent.RAOUT.with.stream.Access   <- 0
-### Animal Densities
-chk.Season.1.Animal.Density.Pasture <- 3.0072900E-03
-chk.Season.1.Animal.Density.Forest  <- 3.0072900E-03
-chk.Season.1.Animal.Density.RAOUT   <- 0.0
-chk.Season.2.Animal.Density.Pasture <- 4.5000000E-02
-chk.Season.2.Animal.Density.Forest  <- 4.5000000E-02
-chk.Season.2.Animal.Density.RAOUT   <- 0.0
 ### animals around streams
 chk.Season.1.Percent.Pasture.in.and.around.streams <- 9.9500000E+00
 chk.Season.1.Percent.Forest.in.and.around.streams  <- 1.9208850E+00
@@ -39,3 +39,66 @@ chk.Season.1.Percent.RAOUT.in.and.around.streams   <- 0
 chk.Season.2.Percent.Pasture.in.and.around.streams <- 9.9500000E+00
 chk.Season.2.Percent.Forest.in.and.around.streams  <- 9.9500000E+00
 chk.Season.2.Percent.RAOUT.in.and.around.streams   <- 0
+## calculations
+## animal numbers
+## season 1
+chk.season.1.Pasture.elk <- chk.Season.1.Animal.Density.Pasture * 
+  chk.Season.1.Pasture
+chk.season.1.Forest.elk <- chk.Season.1.Animal.Density.Forest * 
+  chk.Season.1.Forest
+chk.season.1.RAOUT.elk <- chk.Season.1.Animal.Density.RAOUT * 
+  chk.Season.1.RAOUT
+chk.season.1.total.elk <- chk.season.1.Pasture.elk + chk.season.1.Forest.elk +
+  chk.season.1.RAOUT.elk
+## season 2
+chk.season.2.Pasture.elk <- chk.Season.2.Animal.Density.Pasture * 
+  chk.Season.2.Pasture
+chk.season.2.Forest.elk <- chk.Season.2.Animal.Density.Forest * 
+  chk.Season.2.Forest
+chk.season.2.RAOUT.elk <- chk.Season.2.Animal.Density.RAOUT * 
+  chk.Season.2.RAOUT
+chk.season.2.total.elk <- chk.season.2.Pasture.elk + chk.season.2.Forest.elk +
+  chk.season.2.RAOUT.elk
+## with.without stream access
+## without stream access
+## Season 1
+chk.season.1.Pasture.elk.wo.str.acc <- 
+  (1 - chk.Season.1.Percent.Pasture.with.Stream.Access / 100) * 
+  chk.season.1.Pasture.elk
+chk.season.1.Forest.elk.wo.str.acc <- 
+  (1 - chk.Season.1.Percent.Forest.with.Stream.Access / 100) * 
+  chk.season.1.Forest.elk
+chk.season.1.RAOUT.elk.wo.str.acc <- 
+  (1 - chk.Season.1.Percent.RAOUT.with.Stream.Access / 100) * 
+  chk.season.1.RAOUT.elk
+## Season 2
+chk.season.2.Pasture.elk.wo.str.acc <- 
+  (1 - chk.Season.2.Percent.Pasture.with.Stream.Access / 100) * 
+  chk.season.2.Pasture.elk
+chk.season.2.Forest.elk.wo.str.acc <- 
+  (1 - chk.Season.2.Percent.Forest.with.Stream.Access / 100) * 
+  chk.season.2.Forest.elk
+chk.season.2.RAOUT.elk.wo.str.acc <- 
+  (1 - chk.Season.2.Percent.RAOUT.with.stream.Access / 100) * 
+  chk.season.2.RAOUT.elk
+## with stream access
+## Season 1
+chk.season.1.Pasture.elk.w.str.acc <- 
+  (chk.Season.1.Percent.Pasture.with.Stream.Access / 100) * 
+  chk.season.1.Pasture.elk
+chk.season.1.Forest.elk.w.str.acc <- 
+  (chk.Season.1.Percent.Forest.with.Stream.Access / 100) * 
+  chk.season.1.Forest.elk
+chk.season.1.RAOUT.elk.w.str.acc <- 
+  (chk.Season.1.Percent.RAOUT.with.Stream.Access / 100) * 
+  chk.season.1.RAOUT.elk
+## Season 2
+chk.season.2.Pasture.elk.w.str.acc <- 
+  (chk.Season.2.Percent.Pasture.with.Stream.Access / 100) * 
+  chk.season.2.Pasture.elk
+chk.season.2.Forest.elk.w.str.acc <- 
+  (chk.Season.2.Percent.Forest.with.Stream.Access / 100) * 
+  chk.season.2.Forest.elk
+chk.season.2.RAOUT.elk.w.str.acc <- 
+  (chk.Season.2.Percent.RAOUT.with.stream.Access / 100) * 
+  chk.season.2.RAOUT.elk
