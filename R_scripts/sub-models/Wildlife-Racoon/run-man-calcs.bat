d74e1f10a1d08fdd1c67ad23b08d0fc03fdc8130
kbrannan <brannan.kevin@deq.state.or.us>
2016-03-18 10:49:40 -0700

    created batch file to run manual calc scripts

---
+++ R_scripts/sub-models/Wildlife-Racoon/run-man-calcs.bat
@@ -0,0 +1,17 @@
+@echo off
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-01.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-02.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-04.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-05.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-06.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-07.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-08.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-09.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-10.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-11.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-12.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-13.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-14.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-16.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-17.R
+"C:\Program Files\R\R-3.1.3\bin\x64\Rscript.exe" --vanilla Wildlife_Racoon_Sub_Model_calculation_check-subwtsd-18.R
