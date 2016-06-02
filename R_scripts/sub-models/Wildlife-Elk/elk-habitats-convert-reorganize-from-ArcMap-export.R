21d6ff080c5e52bf2097d419f37c922715c84732
kbrannan <brannan.kevin@deq.state.or.us>
2016-06-02 14:47:40 -0700

    changed the names of seasons from summer/winter to season 1/season 2. re-ran script to get areas with new season names

--- R_scripts/sub-models/Wildlife-Elk/elk-habitats-convert-reorganize-from-ArcMap-export.R
+++ R_scripts/sub-models/Wildlife-Elk/elk-habitats-convert-reorganize-from-ArcMap-export.R
@@ -1,28 +1,24 @@
 
 
 chr.wildlife.elk.dir <- "M:/Models/Bacteria/HSPF/ODEQ-Bacteria-Model-R/R_scripts/sub-models/wildlife-elk"
-chr.habitat.summer.raw.file.name <- "Elk-summer-habitat-ArcMap-Export.txt"
-chr.habitat.winter.raw.file.name <- "Elk-winter-habitat-ArcMap-Export.txt"
+chr.habitat.raw.file.name.season.1 <- "Elk-season-1-habitat-ArcMap-Export.txt"
+chr.habitat.raw.file.name.season.2 <- "Elk-season-2-habitat-ArcMap-Export.txt"
 chr.output <- "elk-Habitat-areas.csv"
 
-df.input.summer.raw <- data.frame(
-  read.csv(paste0(chr.wildlife.elk.dir, "/", chr.habitat.summer.raw.file.name),
+df.raw.season.1 <- data.frame(
+  read.csv(paste0(chr.wildlife.elk.dir, "/", chr.habitat.raw.file.name.season.1),
            stringsAsFactors=FALSE, header=TRUE)[, c("hspf_id", "Seg_Name", "Shape_Area")],
-  season = "summer", stringsAsFactors = FALSE)
+  season = "season 1", stringsAsFactors = FALSE)
 
-names(df.input.summer.raw) <- c("wtsd", "ls", "area", "season")
-
-df.input.winter.raw <- data.frame(
-  read.csv(paste0(chr.wildlife.elk.dir, "/", chr.habitat.winter.raw.file.name),
+df.raw.season.2 <- data.frame(
+  read.csv(paste0(chr.wildlife.elk.dir, "/", chr.habitat.raw.file.name.season.2),
            stringsAsFactors=FALSE, header=TRUE)[, c("hspf_id", "Seg_Name", "Shape_Area")],
-  season = "winter", stringsAsFactors = FALSE)
+  season = "season 2", stringsAsFactors = FALSE)
 
-names(df.input.winter.raw) <- c("wtsd", "ls", "area", "season")
+df.raw <- rbind(df.raw.season.1, df.raw.season.2)
 
-df.raw <- rbind(df.input.winter.raw, df.input.summer.raw)
+names(df.raw) <- c("wtsd", "ls", "area", "season")
 
-
-num.ft.in.mile <- 5280
 num.ft2.in.acre <- 43560
 
 ## data in correct units
